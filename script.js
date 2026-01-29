279f199b26c697979781f51ad488959a2c0249e7
darinka-bebe <hwangdarina@icloud.com>
2026-01-29 20:02:49 +0500

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// ===== СМЕНА ТЕМЫ =====
+
 function setTheme(theme) {
     document.body.classList.remove('theme-girl', 'theme-boy');
     document.body.classList.add('theme-' + theme);
 }
 
-// ===== ЗАГРУЗКА ФОТО =====
+
 function loadImage(event, targetId) {
     const file = event.target.files[0];
     if (!file) return;
@@ -12,13 +12,13 @@ function loadImage(event, targetId) {
     const reader = new FileReader();
 
     reader.onload = function (e) {
-        // большое фото
+       
         const target = document.getElementById(targetId);
         if (target) {
             target.src = e.target.result;
         }
 
-        // кружок сверху
+      
         if (targetId === 'avatarPreview') {
             const headerAvatar = document.getElementById('headerAvatar');
             if (headerAvatar) {
@@ -29,3 +29,4 @@ function loadImage(event, targetId) {
 
     reader.readAsDataURL(file);
 }
+
